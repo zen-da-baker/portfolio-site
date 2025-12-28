01ecb670443d4d56052db1b6dbe39bd7ba68ccb2
Don <120154112+zen-da-baker@users.noreply.github.com>
2025-12-28 13:19:51 -0800

    Express web page mostly working with static file serving

--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -14,8 +14,6 @@ let port = 3007;
 
 app.use( morgan("dev") );
 
-app.use( express.static("../views") );
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
@@ -31,6 +29,8 @@ let resumeFile = "/docs/Resume.pdf";
 
 let relativeFilePath = path.join(__dirname, viewPath);
 
+app.use( express.static( relativeFilePath ) );
+
 app.get("/", async ( req: any, res: any ) => {
 
     let viewFile = await readFile( relativeFilePath + indexFile, "utf-8");
@@ -38,6 +38,8 @@ app.get("/", async ( req: any, res: any ) => {
     res.send( viewFile );
 
 })
+
+/*
 
 app.get("/css/profile.css", async ( req: any, res: any ) => {
 
@@ -51,8 +53,12 @@ app.get("/resume.pdf", async ( req: any, res: any ) => {
 
     let viewFile = await readFile( relativeFilePath + resumeFile );
 
-    res.send( viewFile );
+    res.json( { msg: "hi" } );
+
+    // res.send( viewFile );
 
 })
+
+*/
 
 app.listen(port, () => console.log("App listening to port " + port + "."));
